27617e8e234b5e89c46a72ed73d7d2916607cfea
Murilo Felpeto <murilofelpeto@gmail.com>
2019-07-23 19:43:35 -0300

    Upload to Heroku

--- src/main/java/br/com/murilo/resources/RegistrationResource.java
+++ src/main/java/br/com/murilo/resources/RegistrationResource.java
@@ -14,6 +14,7 @@ import br.com.murilo.dto.UserDTO;
 import br.com.murilo.resources.util.GenericResponse;
 import br.com.murilo.services.UserService;
 
+
 @RestController
 @RequestMapping("/api/public")
 public class RegistrationResource {
